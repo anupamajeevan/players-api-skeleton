c33a40c3b58171dc1964502bbfefe593e8e20037
anupamajeevan <anupamajeevan@gmail.com>
2017-10-23 17:29:49 -0400

    Changed  create player table query

--- migrations/20171006012040_players.js
+++ migrations/20171006012040_players.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.raw(
-    'CREATE TABLE players (id serial primary key,first_name varchar(60) not null unique,last_name varchar(60) not null,rating numeric not null check (rating > 0),handedness varchar(10) check (handedness in (\'right\',\'left\')),created_by varchar(60));'
+    'CREATE TABLE players(id serial primary key,first_name varchar(60) not null,last_name varchar(60) not null,rating numeric not null check (rating > 0),handedness varchar(10) check (handedness in (\'right\',\'left\')),created_by integer, unique (first_name,last_name));'
   );
 };
 
